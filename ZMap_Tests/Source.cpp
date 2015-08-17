37ce84052b71b3185d32b1dc8d81d14406f1eb50
Waty <wartaarnoutse@gmail.com>
2015-08-17 22:32:38 +0200

    Fixed the test project
    Also added a jobname dumper as POC

--- ZMap_Tests/Source.cpp
+++ ZMap_Tests/Source.cpp
@@ -1,34 +1,11 @@
 #include "stdio.h"
 #include "ZMap.h"
 #include <Windows.h>
+#include <fstream>
 
-
-uint32_t CMobPoolPtr = 0x1813928;
-auto GetAt = reinterpret_cast<__POSITION*&(__thiscall*)(ZMap<unsigned long, __POSITION*, unsigned long>, const unsigned long&, __POSITION*&)>(0xC8D1A0);
-
-class CMobPool
-{
-public:
-	virtual ~CMobPool() { };
-
-	ZMap<unsigned long, __POSITION *, unsigned long> m_mMob;
-
-	// we don't care about the other ones atm
-
-	/*	ZList<ZRef<CMob>> m_lMob;
-	ZList<ZRef<CMob>> m_lMobDelayedDead;
-	ZList<ZRef<CMob>> m_lMobDeadProcess;
-	ZRef<CMob> m_pMobDamagedByMob;
-	int m_tLastHitMobDamagedByMob;
-	ZRef<CMob> m_pGuidedMob;
-	unsigned int m_dwMobCrcKey;
-
-	static CMobPool* GetInstance();
-	static int GetCount();
-	CMob* GetMob(unsigned int dwMobId);
-
-	static std::list<CMob*> ValidMobs;*/
-};
+auto jobMap = 0x197BF54;
+auto GetAt = reinterpret_cast<char*&(__thiscall*)(ZMap<uint32_t, char*, uint32_t>*, uint32_t const&, char*&)>(0xEA2910);
+auto get_job_name = reinterpret_cast<char*(__cdecl*)(int jobCodem, uint32_t idk)>(0x5D4500);
 
 void InitConsole()
 {
@@ -43,20 +20,45 @@ void InitConsole()
 	else ShowWindow(handle, SW_SHOW);
 }
 
-DWORD WINAPI main(LPVOID param)
+void save_as_csv(ZMap<uint32_t, char*, uint32_t>* s_mJobName)
+{
+	std::ofstream ostream{ "jobids.csv", std::ofstream::out };
+	ostream << "\"Id\",\"JobName\"";
+	s_mJobName->for_each([&ostream](uint32_t id, char* msg)
+		{
+			ostream << "\"" << msg << "\",\"" << id << "\"\n";
+		});
+}
+
+DWORD WINAPI main(LPVOID /*param*/)
 {
 	InitConsole();
 
-	auto pool = *reinterpret_cast<CMobPool**>(CMobPoolPtr);
-
-	printf("waiting for CMobPool to initialize...");
-	while (!pool) {}
-	printf("CMobPool has initialized, hit any key to start testing");
+	printf("Hit any key to start testing");
 	getchar();
 
-	__POSITION* pos = nullptr;
-	printf("maple GetAt(0) returned %p", GetAt(pool->m_mMob, 0, pos));
-	printf("Waty  GetAt(0) returned %p", pool->m_mMob.get_at(0, pos));
+
+	printf("maple get_job_name(2210,0) returned %s\n", get_job_name(2210, 0));
+
+	ZMap<uint32_t, char*, uint32_t>* s_mJobName = reinterpret_cast<ZMap<uint32_t, char*, uint32_t>*>(0x197BF54);
+	char* result = nullptr;
+	printf("maple GetAt(2210) returned %s\n", GetAt(s_mJobName, 2210, result));
+
+	printf("Waty  GetAt(2210) returned %s\n", *s_mJobName->get_at(2210, result));
+
+	printf("time to test for_each...");
+	getchar();
+
+	s_mJobName->for_each([](uint32_t id, char* msg)
+	{
+		printf("id=%i, value='%s'\n", id, msg);
+	});
+
+	getchar();
+
+	save_as_csv(s_mJobName);
+
+	TerminateProcess(GetCurrentProcess(), 0x1337);
 }
 
 
